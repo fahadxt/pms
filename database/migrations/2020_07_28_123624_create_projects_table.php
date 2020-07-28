476963e7a7dd1242cfe2a1d825fc392090085eeb
Fahad Almutairi <f.s.almotery@gmail.com>
2020-07-28 15:56:00 +0300

    first commet

---
+++ database/migrations/2020_07_28_123624_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('description');
+            $table->integer('owner_id')->unsigned()->comment('user id of the owner of the project');
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
