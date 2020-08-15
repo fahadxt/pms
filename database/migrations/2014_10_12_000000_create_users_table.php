0328ab73f1454f3bffdd198ab3e09e9ddc6033aa
Fahad Almutairi <f.s.almotery@gmail.com>
2020-08-15 21:11:49 +0300

    remove username from users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('username')->unique();
+            // $table->string('username')->unique()->nullable();
             $table->string('bio')->nullable();
             $table->string('designation')->nullable();
             $table->string('avatar')->nullable();
